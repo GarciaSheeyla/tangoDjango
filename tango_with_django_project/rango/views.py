4e54c4ca841ccedfc82e991dd451767351d1d2d8
Sheeyla Garcia <sayla2691@utexas.edu>
2014-07-01 20:51:11 -0500

    html now

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,12 @@
+from django.template import RequestContext
 from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("Rango says hello world!")
+	context = RequestContext(request)
+	context_dict = {'boldmessage': "I am bold font from the context"}
+
+	return render_to_response("Rango says hello world!")
 def about(request):
 	return HttpResponse("Rango Says: Here is the about page.")
 def sheeyla(request):
